4f311ff2c62914d5c3cab6c1273231a4546b9b3b
qinym-d <128761828+qinym-d@users.noreply.github.com>
2024-10-08 20:07:18 +0800

    Create C++ template.cpp

---
+++ C++ template.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 2e5 + 10;
+#define endl '\n'
+#define max(a, b) ((a) > (b) ? (a) : (b))
+#define min(a, b) ((a) < (b) ? (a) : (b))
+string Yes = "Yes", No = "No";
+#define pt(x) cout << (x == 1 ? Yes : No) << endl
+typedef long long ll;
+typedef pair<int, int> pi;
+
+signed main() {
+	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+  
+	return 0;
+}
